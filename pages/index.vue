fde25628a8daae6598f9815a071ebecedd5a57fb
Travis Gertz <trav@louderthanten.com>
2024-09-23 11:32:27 +0800

    Add Home page content

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-    <h1></h1>
-  </div>
+  <header class="container mx-auto pt-12 pb-6 px-2 text-2xl">
+    <h1 class="font-bold text-4xl sm:text-6xl lg:text-9xl">{{ data.homeEntries?.[0]?.title }}</h1>
+    <p v-if="data.homeEntries?.[0]?.pageSubheading">{{ data.homeEntries?.[0]?.pageSubheading }}</p>
+  </header>
+  <section class="page__content">
+    <div class="container mx-auto py-12 px-2 text-balance" v-html="data.homeEntries?.[0]?.pageContent">
+    </div>
+  </section>
 </template>
 
 <script lang="ts" setup>
-  
+  const { data } = await useAsyncGql({
+    operation: 'Home'
+  });
 </script>
