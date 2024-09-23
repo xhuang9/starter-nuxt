d8866d0a6ad5287cc88a063dc9b6911359d3cf35
Travis Gertz <trav@louderthanten.com>
2024-09-23 17:19:12 +0800

    Update gql config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,27 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "nuxt-graphql-client"],
-  runtimeConfig: {
-    public: {
-      GQL_HOST: process.env.GQL_HOST
-    }
+  'graphql-client': {
+    clients: {
+      default: {
+        livePreview: process.env.LIVE_PREVIEW === 'true',
+        host: process.env.GQL_HOST,
+        token: process.env.LIVE_PREVIEW === 'true' ? process.env.AUTH_HEADER : '',
+      },
+      posts: {
+        host: process.env.GQL_HOST,
+        token: process.env.AUTH_HEADER
+      }
+    },
+    tokenStorage: {
+      name: '__session',
+      mode: 'cookie', // default
+      cookieOptions: {
+        path: '/',
+        secure: true, // defaults to `process.env.NODE_ENV === 'production'`
+        httpOnly: false, // Only accessible via HTTP(S)
+        maxAge: 60 * 60 * 24 * 5 // 5 days
+      }
+    } 
   }
 })
