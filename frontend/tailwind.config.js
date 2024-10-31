9ab60050068f63a5b016b0fcd22c8fe0a578d94f
Travis Gertz <trav@louderthanten.com>
2024-10-31 14:36:54 +0800

    Add classes from Craft components into tailwind safelist

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,4 +14,28 @@ export default {
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms')
   ],
+  safelist: [
+    {
+      pattern: /bg-slate-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    {
+      pattern: /rounded-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    {
+      pattern: /text-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    {
+      pattern: /my-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    {
+      pattern: /p-.+/,
+      variants: ['lg', 'md', 'sm']
+    },
+    'aspect-w-16',
+    'aspect-h-9'
+  ]
 }
